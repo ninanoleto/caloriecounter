69e11afa92f821bc8e59cc519e5cf96106cec832
ninanolets <marinaa.noleto@gmail.com>
2021-12-02 21:24:05 +0100

    depoyment commits

--- API-EasyCount/src/output.ts
+++ API-EasyCount/src/output.ts
@@ -3,10 +3,10 @@ import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient();
 
 const main = async () => {
-  const days = await prisma.day.findMany();
+  const days = await prisma.dayPreset.findMany();
   console.log(days);
 
-  const day = await prisma.day.findUnique({ where: { id: 1 } });
+  const day = await prisma.dayPreset.findUnique({ where: { id: 1 } });
   console.log(day);
 };
 
